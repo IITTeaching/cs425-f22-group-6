0898fcd7824391d9ccdac6b945e8528bf32df56a
tbwagner <98062687+tbwagner@users.noreply.github.com>
2022-12-07 12:27:32 -0600

    Updated Project Relational Schema.sql

--- Project Relational Schema.sql
+++ Project Relational Schema.sql
@@ -1,43 +1,49 @@
-﻿CREATE TABLE account (
-  account_id INT PRIMARY KEY,
-  type VARCHAR,
-  balance INT
+CREATE TABLE account ( 
+  account_id INT PRIMARY KEY,  
+  acc_type VARCHAR,  
+  balance NUMERIC 
 );
-
-
-CREATE TABLE transaction (
-  trans_id INT PRIMARY KEY,
-  account_id INT
-  type VARCHAR,
-  amount INT,
-  description VARCHAR,
-  FOREIGN KEY account_id REFERENCES account
-);
-
-
-CREATE TABLE customer (
-  name VARCHAR,
-  address VARCHAR,
-  branch_id INT,
-  account_id INT[], 
-  PRIMARY KEY (name, branch_id),
-  FOREIGN KEY branch_id REFERENCES branch,
-  FOREIGN KEY account_id REFERENCES account
-);
-
 
 CREATE TABLE branch (
   branch_id INT PRIMARY KEY,
   address VARCHAR
 );
 
+CREATE TABLE transaction (
+  trans_id INT PRIMARY KEY,
+  account_id INT,
+  trans_type VARCHAR,
+  amount NUMERIC,
+  date DATE,
+  description VARCHAR,
+  FOREIGN KEY (account_id) REFERENCES account
+);
+
+CREATE TABLE customer (
+  name VARCHAR,
+  address VARCHAR,
+  branch_id INT,
+  PRIMARY KEY (name, branch_id),
+  FOREIGN KEY (branch_id) REFERENCES branch
+);
+
+CREATE TABLE customer_account (
+  name VARCHAR,
+  address VARCHAR,
+  branch_id INT,
+  account_id INT,
+  PRIMARY KEY (name, branch_id, account_id),
+  FOREIGN KEY (name, branch_id) REFERENCES customer,
+  FOREIGN KEY (account_id) REFERENCES account
+);
+
 
 CREATE TABLE employee (
   name VARCHAR,
   branch_id INT,
-  type VARCHAR,
+  emp_type VARCHAR,
   address VARCHAR,
   salary INT,
-  ssn STRING PRIMARY KEY,
-  FOREIGN KEY branch_id REFERENCES branch
+  ssn INT PRIMARY KEY,
+  FOREIGN KEY (branch_id) REFERENCES branch
 );
